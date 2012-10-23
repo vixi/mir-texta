9504ea81ece38051e57e279b95c22231274d896e
yyyurik <7-8@tut.by>
2012-10-23 16:02:33 +0300

    Удалил шапку

--- blocks/block_header_index.php
+++ blocks/block_header_index.php
@@ -1,22 +1,5 @@
 <header>
     <div class=" container_24">
-        <div class="grid_24">
-                <div class="wrapper">
-                <h1><a href="index.php" id="logo_bar"><img src="images/logotype.jpg" width="200px"></a></h1>
-                <div class="fright">
-                <?php
-                if (empty($_SESSION['email']) or empty($_SESSION['id'])) {
-                    echo "<a href='./cabinet.php?action=enter'>Войти</a><br>";
-                    echo "<a href='./cabinet.php?action=registration'>Зарегистрироваться</a><br>";
-                    echo "Вы вошли на сайт, как Гость.";
-                } else {
-                    echo "Вы вошли на сайт, как ".$_SESSION['email']."<br><a href='./cabinet.php?action=cabinet'>В кабинет</a><br>";
-                    echo "<a href='./cabinet.php?action=exit'>Выйти</a>";
-                }
-                ?>
-                </div>
-                <span class="logo-slogan"></span>
-            </div>
             <nav class="main_menu">
                 <ul class="sf-menu">
                     <li><a href="index.php">Заработай</a></li>
