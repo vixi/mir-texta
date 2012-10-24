c124642430ea83c29194cb9e346fe331dc48f5b4
Stratockaster <sushkevichpavel@gmail.com>
2012-10-24 16:16:12 +0200

    Вывод списка пользователей

--- admin/blocks/block_users.php
+++ admin/blocks/block_users.php
@@ -1,3 +1,13 @@
+тут надо бутштраповских тэгов наставить
 <?php
-echo 'users list';
+$admin_users_query = "SELECT id,email FROM users";
+$admin_users_result = mysql_query($admin_users_query);
+while ($admin_users_row = mysql_fetch_array($admin_users_result)) {
+    $admin_users_exit[] = "<a href=./index.php?action=users&user_id=$admin_users_row[0]>$admin_users_row[1]</a><br>";
+}
+echo "<div>";
+foreach ($admin_users_exit as $key => $value) {
+        echo $value;
+}
+echo "</div>";
 ?>
