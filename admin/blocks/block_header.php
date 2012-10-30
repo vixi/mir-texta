b00482c4a1d5da245bb0d14ef2ab90bba4606ad3
Stratockaster <sushkevichpavel@gmail.com>
2012-10-30 11:30:38 +0200

    добавлен скрипт добавления активного класса в меню

--- admin/blocks/block_header.php
+++ admin/blocks/block_header.php
@@ -3,14 +3,20 @@
     <div class="navbar">
       <div class="navbar-inner">
           <ul class="nav">
-            <li class="active">
-              <a href="index.php">Управление сайтом</a>
+            <li>
+                <a href="index.php">Управление сайтом</a>
             </li>
             <li><a href="./articles.php?action=show_all_articles">Статьи <span class="badge badge-info"><?php echo $header->countUserArticles(); ?></span></a></li>
             <li><a href="./users.php">Пользователи  <span class="badge badge-info"><?php echo $header->countUsers(); ?></span></a></li>
             <li><a href="./settings.php">Настройки </a></li>
           </ul>
           <a href="../index.php"><div class="btn" style="float: right;">Выход</div></a>
+          <script>
+          $('div.navbar-inner ul li').each(function () {
+            if (this.getElementsByTagName("a")[0].href == location.href)
+            this.className = "active";
+          });
+        </script>
       </div>
     </div>
 </div>
