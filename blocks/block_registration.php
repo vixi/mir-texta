e441f02de345ec6c2bd8acd7ac86ca33a18310a1
Stratockaster <sushkevichpavel@gmail.com>
2012-11-08 11:03:36 +0200

    реализована базовая javascript валидация форм авторизации

--- blocks/block_registration.php
+++ blocks/block_registration.php
@@ -5,28 +5,28 @@
                 <?php
                 switch($_GET[action]) {
                     case 'enter':
-                      echo "  <form class='grid_10 bs-docs-example form-horizontal' action='./cabinet.php?action=cabinet' method='post'>
+                      echo "<form class='grid_10 bs-docs-example form-horizontal' action='./cabinet.php?action=cabinet' method='post' name='entering' onsubmit='return validate_entering_form();'>
                                 <legend>Вход в личный кабинет</legend>
                                 <div class='control-group'>
-                                <label class='control-label' for='inputEmail'>Ваш Email: </label>
-                                <div class='controls'>
-                                <input type='text' id='inputEmail' placeholder='Email' name='email' size='15' maxlength='25'>
-                                </div>
-                                </div>
-                                <div class='control-group'>
-                                <label class='control-label' for='inputPassword'>Ваш Пароль: </label>
-                                <div class='controls'>
-                                <input id='inputPassword' placeholder='Password' name='password' type='password' size='15' maxlength='25'>
-                                </div>
+                                    <label class='control-label' for='inputEmail'>Ваш Email: </label>
+                                    <div class='controls'>
+                                        <input type='text' id='inputEmail' placeholder='Email' name='email' size='15' maxlength='25'>
+                                    </div>
                                 </div>
                                 <div class='control-group'>
-                                <div class='controls'>
-                                <button type='submit' class='btn' name='submit' value='enter' >Вход</button>
+                                    <label class='control-label' for='inputPassword'>Ваш Пароль: </label>
+                                    <div class='controls'>
+                                        <input id='inputPassword' placeholder='Password' name='password' type='password' size='15' maxlength='25'>
+                                    </div>
                                 </div>
+                                <div class='control-group'>
+                                    <div class='controls'>
+                                        <button type='submit' class='btn' name='submit' value='enter'>Вход</button>
+                                    </div>
                                 </div>
                             </form>";
 
-                      echo "<form class='grid_10 bs-docs-example form-horizontal' style='float:right' action='./cabinet.php?action=cabinet' method='post'>
+                      echo "<form class='grid_10 bs-docs-example form-horizontal' style='float:right' action='./cabinet.php?action=cabinet' method='post' name='registration' onsubmit='return validate_registration_form();'>
                                 <legend>Регистрация на сайте</legend>
                                 <div class='control-group'>
                                 <label class='control-label' for='inputEmail'>Ваш Email:<b>*</b> </label>
@@ -68,7 +68,6 @@
                                 <div class='controls'>
                                 <button id='reg_submit' type='submit' class='btn' name='submit' value='registration'>Регистрация</button>
                                 </div>
-
                             </form>";
                             break;
                 }
@@ -79,10 +78,46 @@
                     }
                 }
                 ?>
-                <script>
+                <script language='javascript' type='text/javascript'>
+                    function validate_entering_form() {
+                        valid = true;
+                        if (document.entering.email.value == '' || document.entering.password.value == '') {
+                            valid = false;
+                            if (document.entering.email.value == '' & document.entering.password.value == '') {
+                                alert("Введите ваш email и пароль");
+                                return valid;
+                            }
+                            if (document.entering.email.value == '') {
+                                alert("Введите ваш email");
+                                return valid;
+                            }
+                            else if (document.entering.password.value == '') {
+                                alert("Введите ваш пароль");
+                                return valid;
+                            }
+                        }
+                        return valid;
+                    }
 
-
-
+                    function validate_registration_form() {
+                        valid = true;
+                        if (document.registration.email.value == '' || document.registration.password.value == '') {
+                            valid = false;
+                            if (document.registration.email.value == '' & document.registration.password.value == '') {
+                                alert("Введите ваш email и пароль");
+                                return valid;
+                            }
+                            if (document.registration.email.value == '') {
+                                alert("Введите ваш email");
+                                return valid;
+                            }
+                            else if (document.registration.password.value == '') {
+                                alert("Введите ваш пароль");
+                                return valid;
+                            }
+                        }
+                        return valid;
+                    }
                 </script>
             </div>
         </div>
